6f8f87cfb7e4ba66fe110cfb6a5885320b3a9e20
Kamil Bacia <kbacia7@gmail.com>
2023-08-07 17:05:08 +0200

    feat(@highland-cattle-chat/shared): added shared consts

---
+++ packages/shared/src/message.ts
@@ -0,0 +1,77 @@
+export const SERVER_PUBLIC_KEY = `-----BEGIN PGP PUBLIC KEY BLOCK-----
+
+mQINBF5LDcsBEADGj6JBqnkSOllARdz5yqrOCr6T1fmYQ2YJR3wVFqrwlnMpWKI1
+I1i4S2o9r3AOBSlDnToptJPbiOl2WjV4ru7gFsAnLJSu/A2lLZAvIEwC4wqx+aFb
+WVs6x60wfkk9innOTDl21NNknz3VTyTVZl0smfwtpPCiKdTSS2uXqnch8SasR5zK
+OBOWexewxmV6aL6gEZy89OBi1TaX1d89b03wmhrADaraaEYsdF/wwVZuvtXYrEKe
+Bc2C5jAYl/QdurAc5/JeRf7wJ6sTCsJfcdpz8LwJCZEXhg3xA3fWyYsCMp4WEq3A
+KdUiiHQy41vK4sQnWUmapM/N1jk88k4Bh3JFEvqp/81KRTfCMUUXN2P4+O2aqLVF
+bSHqK8DDTeL1w8v5UxTWgxEegNZwgVjPcGl1YnS/WPQM2ZoS79qGS3P6yHDf9hEY
+jQEd840y7cYelAF/dLgzvKRQzlzAQNX47ps7Kns6Cgy3bSK0Vh5pLIgjYkeg83+j
+VLzCtmLHTwRxpVp7amIlxGSToTmMTPIYIYhgRrN+I3zmBv5mu9AnWtfUiwZ8u3nJ
+XPirPCWQfqrwAVCOzXLGPgIQXXFpU11E9Y0jJFCBgaWbl7lDdL1Cx1fJV+P9f06D
+hcS7M8CkaNXn3RQggKU9wNTe+P4Kauo9z2ELC4qD/QBZnrBSYqNURnRJCwARAQAB
+tB9rZXJvcmVybyA8a2Vyb3Jlcm8xM0BnbWFpbC5jb20+iQJOBBMBCgA4FiEEt4HV
+63NkyUgteCjlXQ+37fLHG7cFAl5LDcsCGwMFCwkIBwIGFQoJCAsCBBYCAwECHgEC
+F4AACgkQXQ+37fLHG7fKBQ/+O8rLCldQEQeCVG07aurtaFRQv0MsW0Q1LRY/SM32
+Aa4puPYfMQPYzEM5WxZR5nevhDYOAJsk6PkBa4Jl6G8xfKjX/KZOghRahlKkcJJY
+IU9OsJkwKw2tgWqcZy4PyVaRm2P77TLrTYL+aM6D5Ymmjl65Lapu8hHz0r1P9GYR
+DQy+xlnQCdbpVwzV9oegxH1qxBxyrU/FcUO+Ww/0KdEDqZReSXxPR1XqApMmPZWI
+dn0R95Y+PX/GQy0BZnsn254saZQK+4jhOBWAXfyQ/5eZSetMBRpwpdEROf2t2Gcw
+CGm0/CraCsegeo6qglqz6D9S1PB7BCyin+oiC+2GR+Y7HiupCW21QJ8pA7elL+NO
+Xzdf4pY1o7mpAvUREa6VkRQw5VfoCCZ2B3KwAYZQx9EEQd7qnimx5N6tKhH0VqZI
+cU88CwSLHa/5zU0C0ZENtUHbSukz/6iQ8IZfMdmlR4TRdVNFq+ezmRsgzrUvCa6l
+elofB1BRI7Eind6G+gfELXKvhCaDUzHj0VEDlQQSHoEuEn5hsSrNBvnB1fbPtZUv
+PBm3rjxw0E83BZtab5nq4Jo9lLVSLG+iP8e17hfu/N+uUxvgI5LnKmtu75DiFAl8
+SQgkqj4FxaKZ+VT0BfU90UJU2MsXGYjFcwZ0oinnBqYdKpOgEa7qqR+Q3/0E4Lbh
+9ma5Ag0EXksNywEQAMpxhk2mph0w7sAFs9JJK8wxhtjZAU+uu8ALqFG8K7azfcge
+vXvyQ68TP8vhRn/A62HhhQatpNIGc+qjFYjr2j2RjuKALHNw8M3Ni3pVz0lT4tbw
+e0dQ7VLv5MLV05mBXQyIQsBfL1lPNOIwW/ApkI9tu6omdoqX/iqVcA6aZJooOabR
+XHkuP417ZNxQLVv+jFGWuzBOZj9onC8TdOTdUCW+Cj1Jai5qZmZLkNMEHqPmMpTI
+CTWZVPPYoord86+SsiBR/9JN85xeibVHqxWRebwtAymUIYtY84WFwiXIXHT1JyN0
+BxGfq4Tl6/59eBgmzpPz9BMrnyUR8Udmhm+tG6BiacP8srHITsmNdA5t0X6qdG12
+K+/NaQOWz0/S/ErWUQRUvyHidT1hZ6SoajykvzISvqWk1sfOTuG2hkVhyGdd4HkC
+6kZJY0QHTL5O1LG0FtbZQORrA/uWDbtEPxLUmxk48LspJq0nKcvgUPMg4C4c45Zr
+pCyZtMzC8FS7v9fuUDJN2BA/P9xTgsP3LoE4JD0oN+MDchhGJgX5xTVh0Xp9GuQ4
+nadt66g41ZC6WQ2jvOxTI7G2EKLLFIJM5+/HsqNgn/HhJtU0DbQzOhV1PT1KSxzH
+lxXsInDvw8ulppVD2jqLcROPvDwq/FOY/fPVwZ+potKUh/eLvGu9yW4VF7hHABEB
+AAGJAjYEGAEKACAWIQS3gdXrc2TJSC14KOVdD7ft8scbtwUCXksNywIbDAAKCRBd
+D7ft8scbtycGD/9bLxoYVeqSljRFnohNHi/majI5NJw3+cDP9aVkM9CYzLLQ6bA0
+qdZuxRR8EPr+zSlnTzJkgfirhwndk54KiffrZODuEm123xUdTVuxIhp+Mt1obT0a
+5CZuzyid5gAX7uDVZtZ/FpoZQSRB3moNK30qSLz3Cf96Aoj1hWJG9/z5aDklu3V4
+i3YaRu4P1cJkgSAuJOg++eHXoZIZAwTbuhkQfjlmso0lWaQE/ZAoZmMGLvGEut5d
+PmjNs9GzywglThM+HLF9BW/GuDOv+wtijWwEBLJuefevFi+OHxLTFDmhg91N7cv1
+IiGbTgzzlrLIIbR5Z2+YeMCmXX5XlUGb9DnoeruYQAsLTo1fU2fSIEWnIKpaIJeM
+BcjT8k9ZgteVNQiUNVVnl14Z7quch5oxIJ+C5tfPzKoqKDmoGQhHllyUIusoJj+2
+zXh+oMP8BGNhBq3F6kgHRf+adde1Ua+2iax9IDGMmuN0wo1fJ0qsIg5BBk5rI/pO
+MjhAXvkX4vfv3KD8Fe0+vjENkXo0yZQjEgQTleW7h5RZuYpRTrbdm4p7QwBURNzc
+7XDFxQHmRBlftAdbwvjR6jVjt6Ntku/5fTZKYTimUcy1m6TOlrAHWQ+I+e/Q23fi
+uXUyfnGA3p3qaWcMmght0gKbxpqxuDaDuFB3Hj6+oiUN+vD+Cq+Gnr/uIQ==
+=U8YM
+-----END PGP PUBLIC KEY BLOCK-----
+`;
+
+export const MessageTypes = {
+  INIT: "INIT",
+  TEXT: "TEXT",
+  UNKNOWN_ERROR: "UNKNOWN_ERROR",
+} as const;
+
+export const MessageStatuses = {
+  OK: "OK",
+  ERROR: "ERROR",
+} as const;
+
+export type MessageType = (typeof MessageTypes)[keyof typeof MessageTypes];
+export type MessageStatus =
+  (typeof MessageStatuses)[keyof typeof MessageStatuses];
+export type IncomeMessage = {
+  type: MessageType;
+  senderPublicKey: string;
+  recipientPublicKey?: string;
+  content?: string;
+};
+
+export interface OutcomeMessage extends IncomeMessage {
+  status: MessageStatus;
+}
