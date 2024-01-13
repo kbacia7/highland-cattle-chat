118ad830c4c673aadd14bab648a098c9dea61e81
Kamil Bacia <kbacia7@gmail.com>
2024-01-13 09:28:37 +0100

    fix(schema): added displayName to register schema

--- packages/shared/src/schemas.ts
+++ packages/shared/src/schemas.ts
@@ -1,7 +1,23 @@
 import { z } from "zod";
 
+export const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(8, {
+    message: "Password must be at least 8 characters",
+  }),
+});
+
 export const registerSchema = z
   .object({
+    displayName: z
+      .string()
+      .refine(
+        (value) => value.length > 3 && value.match(/^[a-zA-Z0-9_@ -]+$/),
+        {
+          message:
+            "Display name must be at least 3 characters, can contains only letters, numbers, space, and _@-",
+        }
+      ),
     email: z.string().email(),
     password: z.string().min(8, {
       message: "Password must be at least 8 characters",
