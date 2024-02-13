a863205e7daee898821375878699e7b74a8706f7
Kamil Bacia <kbacia7@gmail.com>
2024-02-13 08:36:35 +0100

    feat(Nav): added input to search users

--- packages/web-client/src/components/Nav.tsx
+++ packages/web-client/src/components/Nav.tsx
@@ -1,48 +1,138 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
+
+import { searchUserSchema } from "@highland-cattle-chat/shared";
 
 import { USER_STATUS } from "~/consts";
-import { useAppSelector } from "~/slices/hooks";
+import {
+  useCreateConversationMutation,
+  useSearchUserQuery,
+} from "~/slices/conversationsSlice";
 
 import Conversation from "./Conversation";
 import SettingsIcon from "./icons/Settings";
 
-import type { LoadConversationsResponse } from "@highland-cattle-chat/shared";
+import Input from "./Input";
+
+import { Alert } from "./Alert";
+
+import type { InputProps } from "./Input";
+
+const SearchInput = ({
+  onSearch,
+  ...props
+}: {
+  onSearch: (value: string) => void;
+} & InputProps) => {
+  const onChange = useDebouncedCallback((value) => onSearch(value), 500);
+
+  return (
+    <Input
+      placeholder={"Search by display name..."}
+      onChange={(e) => onChange(e.target.value)}
+      {...props}
+    />
+  );
+};
+
+type ConversationItemProps = {
+  id: string;
+  displayName: string;
+  image: string;
+};
 
 type Props = {
-  conversations: LoadConversationsResponse;
+  conversations: ConversationItemProps[];
+};
+
+const ConversationsList = ({ conversations }: Props) => (
+  <ul className="flex flex-col items-center">
+    {conversations?.map(({ id, displayName, image }) => (
+      <Link to={`conversation/${id}`} className="w-full" key={id}>
+        <Conversation
+          lastMessage="text"
+          status={USER_STATUS.ONLINE}
+          title={displayName}
+          image={image}
+        />
+      </Link>
+    ))}
+  </ul>
+);
+
+const UsersList = ({ phrase }: { phrase: string }) => {
+  const parseResults = searchUserSchema.safeParse({ phrase });
+  const [createConversation] = useCreateConversationMutation();
+  const { currentData: conversations, isError } = useSearchUserQuery(
+    { phrase },
+    { skip: !parseResults.success },
+  );
+  const navigate = useNavigate();
+
+  const onClick = async (id: string) => {
+    try {
+      const res = await createConversation({ id }).unwrap();
+      navigate(`conversation/${res.id}`);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <>
+      {(isError || !parseResults.success) && (
+        <Alert className="mt-2" type="error">
+          <span>
+            {!parseResults?.success
+              ? parseResults.error.format().phrase?._errors.join("")
+              : "Unknown error"}
+          </span>
+        </Alert>
+      )}
+
+      <ul className="flex flex-col items-center">
+        {conversations?.map(({ id, displayName, image }) => (
+          <Link to="#" key={id} className="w-full" onClick={() => onClick(id)}>
+            <Conversation
+              status={USER_STATUS.UNKNOWN}
+              title={displayName}
+              image={image}
+            />
+          </Link>
+        ))}
+      </ul>
+    </>
+  );
 };
 
 const Nav = ({ conversations }: Props) => {
-  const { userId } = useAppSelector((state) => state.loggedUser);
+  const [searchPhrase, setSearchPhrase] = useState<string>();
+
+  const onSearch = async (phrase: string) => {
+    setSearchPhrase(phrase);
+  };
 
   return (
     <nav className="h-full bg-blue-100 min-w-[400px] max-w-[400px]">
-      <div className="flex flex-row align-baseline mx-4 py-2">
-        <h2 className="text-3xl font-bold text-blue-900 inline">
-          Conversations
-        </h2>
-        <button>
-          <SettingsIcon className="inline ml-3" size={36} />
-        </button>
+      <div className="mx-4">
+        <div className="flex flex-row align-baseline py-2">
+          <h2 className="text-3xl font-bold text-blue-900 inline">
+            Conversations
+          </h2>
+          <button>
+            <SettingsIcon className="inline ml-3" size={36} />
+          </button>
+        </div>
+
+        <SearchInput color="white" onSearch={onSearch} />
       </div>
 
-      <ul className="flex flex-col items-center">
-        {conversations?.map(({ id, title, participants }) => {
-          const image = participants.find((p) => p.user.id != userId)?.user
-            .image;
+      {!!searchPhrase?.length && <UsersList phrase={searchPhrase || ""} />}
 
-          return (
-            <Link to={`conversation/${id}`} className="w-full" key={id}>
-              <Conversation
-                lastMessage="text"
-                status={USER_STATUS.ONLINE}
-                title={title}
-                image={image || ""}
-              />
-            </Link>
-          );
-        })}
-      </ul>
+      {!searchPhrase?.length && (
+        <ConversationsList conversations={conversations} />
+      )}
     </nav>
   );
 };
