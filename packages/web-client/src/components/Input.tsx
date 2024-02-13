ebbe4c0a51fa01c62c1b60d1846fa72b187f2f6f
Kamil Bacia <kbacia7@gmail.com>
2024-02-13 08:35:27 +0100

    feat(Input): added colors Input

--- packages/web-client/src/components/Input.tsx
+++ packages/web-client/src/components/Input.tsx
@@ -1,20 +1,23 @@
 import { cx } from "class-variance-authority";
 import { forwardRef } from "react";
 
-interface Props extends React.ComponentPropsWithRef<"input"> {
+export interface InputProps extends React.ComponentPropsWithRef<"input"> {
   error?: boolean;
+  color?: "primary" | "white";
 }
 
-const TextInput = forwardRef<HTMLInputElement, Props>(
-  ({ error, ...props }, inputRef) => (
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ error, color = "primary", ...props }, inputRef) => (
     <input
       ref={inputRef}
       type="text"
       className={cx(
-        "p-5 rounded-full bg-blue-100 placeholder:text-gray-500 border-gray-100 focus:border-blue-200 border-2 h-11 w-full outline-none focus:drop-shadow-md focus:ring-1",
+        "p-5 rounded-full placeholder:text-gray-500 border-gray-100 focus:border-blue-200 border-2 h-11 w-full outline-none focus:drop-shadow-md focus:ring-1",
         {
           "border-red-300 bg-red-100 font-bold text-red-700 placeholder:text-red-500":
             error,
+          "bg-blue-100": color === "primary" && !error,
+          "bg-white": color === "white" && !error,
         },
       )}
       {...props}
@@ -22,4 +25,4 @@ const TextInput = forwardRef<HTMLInputElement, Props>(
   ),
 );
 
-export default TextInput;
+export default Input;
