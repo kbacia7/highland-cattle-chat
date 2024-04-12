4ff42204908566e7a7412c3d5890358bb6556543
Kamil Bacia <kbacia7@gmail.com>
2024-04-12 17:23:20 +0200

    fix(sendWelcomeMessages): add delay to messages from bot

--- packages/server/src/helpers/sendWelcomeMessages.ts
+++ packages/server/src/helpers/sendWelcomeMessages.ts
@@ -4,6 +4,15 @@ const sendWelcomeMessages = async (
   fastify: FastifyInstance,
   recipientId: string,
 ) => {
+  const now = new Date();
+  const welcomeMessages = [
+    "Welcome to the chat!",
+    "It's one of my portfolio projects, full README you can find in repository https://github.com/kbacia7/highland-cattle-chat",
+    "At this moment you can search people by using input on the left, but as you can guess, there's no many accounts. App also support for sending images",
+    "Project have a tests written in vitest, I wrote it with scalablity in mind, app uses MongoDB to store data, Redis for cache and Websockets for real-time communication",
+    "In repository you'll find more info about how to run it and technologies, but I used also a.o.: Typescript, Fastify, React, Prisma, Tailwind",
+  ];
+
   await fastify.prisma.conversation.create({
     data: {
       title: "Mrs. Guide",
@@ -19,32 +28,11 @@ const sendWelcomeMessages = async (
       },
       messages: {
         createMany: {
-          data: [
-            {
-              userId: fastify.guideUser.id,
-              content: "Welcome to the chat!",
-            },
-            {
-              userId: fastify.guideUser.id,
-              content:
-                "It's one of my portfolio projects, full README you can find in repository https://github.com/kbacia7/highland-cattle-chat",
-            },
-            {
-              userId: fastify.guideUser.id,
-              content:
-                "At this moment you can search people by using input on the left, but as you can guess, there's no many accounts. Here's also support for sending images",
-            },
-            {
-              userId: fastify.guideUser.id,
-              content:
-                "Project have a lot of tests written in Jest and Cypress, I wrote it with scalablity in mind, app uses MongoDB to store data, Redis for cache and Websockets for real-time communication",
-            },
-            {
-              userId: fastify.guideUser.id,
-              content:
-                "In repository you'll find more info about how to run it and technologies, but I used also a.o.: Typescript, Fastify, React, Prisma, Tailwind",
-            },
-          ],
+          data: welcomeMessages.map((content, extraMs) => ({
+            userId: fastify.guideUser.id,
+            content,
+            createdAt: new Date(now.getTime() + extraMs),
+          })),
         },
       },
     },
