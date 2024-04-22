cc7807c7c23512deb2ec798130e41e5786146ba5
Kamil Bacia <kbacia7@gmail.com>
2024-04-22 09:16:13 +0200

    fix(web-worker): initialize socket after load web worker

--- packages/web-client/src/web-worker.ts
+++ packages/web-client/src/web-worker.ts
@@ -1,11 +1,5 @@
 import { MessageTypes } from "@highland-cattle-chat/shared";
-import { get } from "idb-keyval";
 
-import { InternalMessageTypes } from "./consts/broadcast";
-
-import { USER_ID_KEY_ITEM_NAME } from "./utils/localStorage";
-
-import type { InternalMessage } from "./consts/broadcast";
 import type {
   IncomeMessage,
   OutcomeMessage,
@@ -18,14 +12,8 @@ const oldHistory: OutcomeMessage[] = [];
 
 const receivedChannel = new BroadcastChannel("received_messages");
 const sendedChannel = new BroadcastChannel("sended_messages");
-const internalChannel = new BroadcastChannel("internal_messages");
 
 const initializeSocket = async () => {
-  const USER_ID = await get<string>(USER_ID_KEY_ITEM_NAME);
-  if (!USER_ID) {
-    throw new Error("Oops, USER_ID is missing");
-  }
-
   socket = new WebSocket(`${import.meta.env.VITE_WSS_ENDPOINT}/real-time`);
   socket.addEventListener("message", async (event) => {
     const message: OutcomeMessage = JSON.parse(event.data);
@@ -36,7 +24,6 @@ const initializeSocket = async () => {
   socket.addEventListener("open", async () => {
     const initMessage: IncomeMessage = {
       type: MessageTypes.INIT,
-      userId: USER_ID,
     };
 
     socket.send(JSON.stringify(initMessage));
@@ -52,15 +39,6 @@ sendedChannel.addEventListener("message", async (event) => {
   socket.send(JSON.stringify(message));
 });
 
-internalChannel.addEventListener("message", async (event) => {
-  if (broadcastChannelActive) return;
-  const message: InternalMessage = event.data;
-  switch (message.type) {
-    case InternalMessageTypes.READY: {
-      initializeSocket();
-      broadcastChannelActive = true;
-      sendedChannel.postMessage(oldHistory);
-      break;
-    }
-  }
-});
+initializeSocket();
+broadcastChannelActive = true;
+sendedChannel.postMessage(oldHistory);
