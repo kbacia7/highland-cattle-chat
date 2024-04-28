2d6291ca2816df1e71290b1fef11e9e5ff987c42
Kamil Bacia <kbacia7@gmail.com>
2024-04-28 17:00:39 +0200

    fix(vite.config): use cert pathes from env

--- packages/web-client/vite.config.ts
+++ packages/web-client/vite.config.ts
@@ -1,22 +1,25 @@
 import fs from "fs";
-import path from "path";
 
-import { defineConfig, splitVendorChunkPlugin } from "vite";
+import { defineConfig, splitVendorChunkPlugin, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths(), splitVendorChunkPlugin()],
-  build: {
-    target: "esnext",
-  },
-  worker: {
-    format: "es",
-  },
-  server: {
-    https: {
-      key: fs.readFileSync(path.join("./https", "localhost-key.pem")),
-      cert: fs.readFileSync(path.join("./https", "localhost.pem")),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react(), tsconfigPaths(), splitVendorChunkPlugin()],
+    build: {
+      target: "esnext",
     },
-  },
+    worker: {
+      format: "es",
+    },
+    server: {
+      https: {
+        key: fs.readFileSync(env.HTTPS_KEY_PATH),
+        cert: fs.readFileSync(env.HTTPS_CERT_PATH),
+      },
+    },
+  };
 });
